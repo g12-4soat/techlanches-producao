52a366841f2caee9540af536aa6c48234d524e58
aoliveiramateus <mateusaragao@hotmail.com>
2024-05-08 20:54:01 -0300

    fix sonar

--- src/TechLanches.Producao/Core/TechLanches.Producao.Application/Controllers/FilaPedidoController.cs
+++ src/TechLanches.Producao/Core/TechLanches.Producao.Application/Controllers/FilaPedidoController.cs
@@ -24,19 +24,19 @@ namespace TechLanches.Producao.Application.Controllers
         {
             _logger.LogInformation("FilaPedidosHostedService iniciado: {time}", DateTimeOffset.Now);
 
-            _logger.LogInformation("Próximo pedido da fila: {proximoPedido.Id}", message.Id);
+            _logger.LogInformation("Próximo pedido da fila: {proximoPedidoId}", message.Id);
 
             await _pedidoController.TrocarStatus(message.Id, StatusPedido.PedidoEmPreparacao);
 
-            _logger.LogInformation("Pedido {proximoPedido.Id} em preparação.", message.Id);
+            _logger.LogInformation("Pedido {proximoPedidoId} em preparação.", message.Id);
 
             await Task.Delay(1000 * _workerOptions.DelayPreparacaoPedidoEmSegundos);
 
-            _logger.LogInformation("Pedido {proximoPedido.Id} preparação finalizada.", message.Id);
+            _logger.LogInformation("Pedido {proximoPedidoId} preparação finalizada.", message.Id);
 
             await _pedidoController.TrocarStatus(message.Id, StatusPedido.PedidoPronto);
 
-            _logger.LogInformation("Pedido {proximoPedido.Id} pronto.", message.Id);
+            _logger.LogInformation("Pedido {proximoPedidoId} pronto.", message.Id);
         }
     }
 }
